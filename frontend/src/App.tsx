eaa45d5d093c2cfc1ceedf0f3715935c38ac63d7
sazonov <fromlhz03@gmail.com>
2022-07-01 12:53:39 +0300

    remove useless code,fix func work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,25 @@
-import { getApolloContext, useQuery } from "@apollo/client";
 import { FC } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { ErrorLayout } from "./components/ErrorLayout";
 import { Login } from "./components/Login";
 import { MainPageLayout } from "./components/MainPageLayout";
 import { Unauthorized } from "./components/Unauthorized";
-import { VALIDATE_TOKEN } from "./graphql/queries/validateToken";
 import { Home } from "./pages/Home/Home";
 import { Account } from "./pages/Account/Account";
 import { Bills } from "./pages/Bills/Bills";
 import { Info } from "./pages/Info/Info";
 
-export const App: FC = () => {
-  const navigate = useNavigate();
-
-  const context = getApolloContext();
-
-  console.log("context ===========>: ", context);
-  function navigateToLoginPage() {
-    navigate("unauthorized");
-    const redirect = setTimeout(() => navigate("login"), 3000);
-    return () => clearTimeout(redirect);
-  }
-
-  const { loading, data } = useQuery(VALIDATE_TOKEN, {
-    onError: () => navigateToLoginPage(),
-  });
-
-  if (loading) return <p>Loading...</p>;
-
-  console.log("data ===========>: ", data);
-
-  return (
-    <Routes>
-      <Route path="/" element={<ErrorLayout />}>
-        <Route path="unauthorized" element={<Unauthorized />} />
-        <Route path="login" element={<Login />} />
-      </Route>
-      <Route path="/main" element={<MainPageLayout />}>
-        <Route path="home" element={<Home />} />
-        <Route path="account" element={<Account />} />
-        <Route path="bills" element={<Bills />} />
-        <Route path="info" element={<Info />} />
-      </Route>
-    </Routes>
-  );
-};
+export const App: FC = () => (
+  <Routes>
+    <Route path="/" element={<ErrorLayout />}>
+      <Route path="unauthorized" element={<Unauthorized />} />
+      <Route path="login" element={<Login />} />
+    </Route>
+    <Route path="/main" element={<MainPageLayout />}>
+      <Route path="home" element={<Home />} />
+      <Route path="account" element={<Account />} />
+      <Route path="bills" element={<Bills />} />
+      <Route path="info" element={<Info />} />
+    </Route>
+  </Routes>
+);
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,8 +1,9 @@
 import { useMutation } from "@apollo/client";
-import { FC, useState } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { LoginUser } from "../graphql/inputTypes/loginUser";
 import { LOGIN } from "../graphql/mutations/login";
+import { LoggedUser } from "../graphql/types/user";
 
 export const Login: FC = () => {
   const navigate = useNavigate();
@@ -11,29 +12,42 @@ export const Login: FC = () => {
     password: "",
   });
 
-  const [login] = useMutation<LoginUser>(LOGIN, {
+  const [login] = useMutation<LoggedUser>(LOGIN, {
     variables: {
       data: {
-        email: loginFields.email,
-        password: loginFields.password,
+        email: loginFields.email.trim(),
+        password: loginFields.password.trim(),
       },
     },
-    onCompleted: () => navigate("/"),
-    onError: () => navigate("unauthorized"),
+    onCompleted: (data) => {
+      const { loginUser } = data;
+      if (loginUser.token.accessToken) {
+        window.localStorage.setItem("accessToken", JSON.stringify(loginUser.token.accessToken));
+      }
+      navigate("/");
+    },
+    onError: () => {
+      navigate("/unauthorized");
+      setTimeout(() => navigate("/login"), 3000);
+    },
   });
 
-  function changeLogin(value: string) {
+  function changeLogin(event: ChangeEvent<HTMLInputElement>) {
+    const { target } = event;
+    const { value } = target;
     setLoginFields((prevState) => ({ ...prevState, email: value }));
   }
 
-  function changePassword(value: string) {
+  function changePassword(event: ChangeEvent<HTMLInputElement>) {
+    const { target } = event;
+    const { value } = target;
     setLoginFields((prevState) => ({ ...prevState, password: value }));
   }
 
   return (
     <form style={{ display: "flex", flexDirection: "column", maxWidth: "15rem" }}>
-      <input type="text" value={loginFields.email} onChange={({ target: { value } }) => changeLogin(value)} />
-      <input type="text" value={loginFields.password} onChange={({ target: { value } }) => changePassword(value)} />
+      <input type="text" value={loginFields.email} onChange={changeLogin} />
+      <input type="text" value={loginFields.password} onChange={changePassword} />
       <button
         type="submit"
         onClick={(event) => {
