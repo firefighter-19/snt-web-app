8f317bc7ea587419566812a8c1b4057ee7f333aa
sazonov <fromlhz03@gmail.com>
2022-07-26 18:32:51 +0300

    fix header component styles

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,24 +1,27 @@
 import { FC } from "react";
 import { NavLink } from "react-router-dom";
+import { Container } from "../Container/Container";
 import classes from "./Header.module.scss";
 
 export const Header: FC = () => {
-  const IS_USER_LOGGED_IN = false;
+  const IS_USER_LOGGED_IN = !!window.localStorage.getItem("refreshToken");
   return (
-    <div className={classes.header__container}>
+    <Container>
       <header className={classes.header}>
-        <h1 className={classes.header_name}>СНТ Энциклопедист</h1>
-        <p className={classes.header_description}>Московская область, Солнечногорский район, д. Носово</p>
+        <div className={classes.header__block}>
+          <h1 className={classes.header_name}>СНТ Энциклопедист</h1>
+          <p className={classes.header_description}>Московская область, Солнечногорский район, д. Носово</p>
+        </div>
+        {IS_USER_LOGGED_IN ? (
+          <NavLink to="/account" title="Аккаунт">
+            X
+          </NavLink>
+        ) : (
+          <NavLink to="/login" title="Войти" className={classes.header__link}>
+            Войти
+          </NavLink>
+        )}
       </header>
-      {IS_USER_LOGGED_IN ? (
-        <NavLink to="/account" title="Аккаунт">
-          X
-        </NavLink>
-      ) : (
-        <NavLink to="/login" title="Войти" className={classes.header__link}>
-          Войти
-        </NavLink>
-      )}
-    </div>
+    </Container>
   );
 };
