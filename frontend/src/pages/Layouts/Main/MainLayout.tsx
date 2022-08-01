77c498a4f17c84aa13ed28c67c6965bd0710a343
sazonov <fromlhz03@gmail.com>
2022-08-01 18:28:46 +0300

    update MainLayout and its styles

--- frontend/src/pages/Layouts/Main/MainLayout.tsx
+++ frontend/src/pages/Layouts/Main/MainLayout.tsx
@@ -1,13 +1,11 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "../../../components/Header/Header";
-import { NavBar } from "../../../components/Navigation/NavBar";
 import classes from "./MainLayout.module.scss";
 
 export const MainLayout: FC = () => (
   <div className={classes.mainPageLayout}>
     <Header />
-    <NavBar />
     <Outlet />
   </div>
 );
