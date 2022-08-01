37201c35af50caafca2afe86854d2b45e59ff257
sazonov <fromlhz03@gmail.com>
2022-08-01 18:27:04 +0300

    update user types for register

--- frontend/src/graphql/types/user.ts
+++ frontend/src/graphql/types/user.ts
@@ -4,7 +4,6 @@ import { RefreshToken } from "./token";
 export interface User {
   id: string;
   email: string;
-  password: string;
   name: string;
   lastName: string;
   siteNumber: number;
@@ -16,4 +15,12 @@ export interface User {
 
 export interface LoggedUser {
   loginUser: User;
+}
+
+export interface RegisterUser {
+  registration: User;
+}
+
+export interface UserInfo {
+  getUser: User;
 }
