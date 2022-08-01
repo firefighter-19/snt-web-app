50b3998546322b0bdb38b28f4c19c86939a91ac6
sazonov <fromlhz03@gmail.com>
2022-08-01 18:27:15 +0300

    create account page

--- frontend/src/pages/Account/Account.tsx
+++ frontend/src/pages/Account/Account.tsx
@@ -1,8 +1,33 @@
-import { FC } from "react";
-import { User } from "../../graphql/types/user";
+import { UserInfo } from "../../graphql/types/user";
 
-interface Props {
-  data: User | undefined;
-}
-
-export const Account: FC<Props> = ({ data }) => <div>Hello React Typescript</div>;
+export const Account = ({ getUser: user }: UserInfo): JSX.Element => {
+  const { name, lastName, siteNumber, role: roles } = user;
+  return (
+    <div>
+      <div>
+        <div>Личные данные:</div>
+        <div>Аватар</div>
+        <div>
+          <p>Имя:</p>
+          <p>{name}</p>
+        </div>
+        <div>
+          <p>Фамилия:</p>
+          <p>{lastName}</p>
+        </div>
+        <div>
+          <p>Номер участка:</p>
+          <p>{siteNumber}</p>
+        </div>
+        <div>
+          <p>Статус:</p>
+          <div>
+            {roles?.map(({ id, role }) => (
+              <p key={id}>{role}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
