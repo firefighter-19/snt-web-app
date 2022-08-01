369dd8c8f42566f644f5264cd97f1ca22e8508ba
sazonov <fromlhz03@gmail.com>
2022-08-01 18:25:37 +0300

    add hoc to routes array

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,15 +1,21 @@
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
+import { UserInfo } from "./graphql/types/user";
+import { Account } from "./pages/Account/Account";
 import { AccountQuery } from "./pages/Account/AccoutQuery";
 import { Bills } from "./pages/Bills/Bills";
 import { Home } from "./pages/Home/Home";
 import { Info } from "./pages/Info/Info";
+import { Login } from "./pages/Login/Login";
+import { Registration } from "./pages/Registration/Registration";
 
 export interface Routes {
   id: string;
   element: ReactNode;
   path: string;
 }
+
+const returnPreview = ({ getUser }: UserInfo) => <Account getUser={getUser} />;
 
 export const mainRoutes: Routes[] = [
   {
@@ -55,10 +61,20 @@ export const mainRoutes: Routes[] = [
   {
     id: "11",
     path: "/account",
-    element: <AccountQuery />,
+    element: <AccountQuery ChildComponent={returnPreview} />,
   },
   {
-    id: "11",
+    id: "12",
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    id: "13",
+    path: "/registration",
+    element: <Registration />,
+  },
+  {
+    id: "14",
     path: "*",
     element: <Navigate to="/" replace />,
   },
