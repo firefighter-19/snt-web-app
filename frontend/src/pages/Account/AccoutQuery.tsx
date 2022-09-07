afe7aa6e5de29416089ea2e2ff74ff4142281646
sazonov <fromlhz03@gmail.com>
2022-09-07 23:25:03 +0300

    edit accountQuery

--- frontend/src/pages/Account/AccoutQuery.tsx
+++ frontend/src/pages/Account/AccoutQuery.tsx
@@ -1,19 +1,20 @@
 import { useQuery } from "@apollo/client";
 import { ElementType, FC } from "react";
-import { GET_USER } from "../../graphql/queries/getUser";
-import { User, UserInfo } from "../../graphql/types/user";
+import { GET_USER } from "src/graphql/queries/getUser";
+import { User, UserInfo } from "src/graphql/types/user";
 
 interface Props {
   ChildComponent: ElementType<UserInfo>;
+  ErrorComponent: ElementType;
 }
 
-export const AccountQuery: FC<Props> = ({ ChildComponent }) => {
+export const AccountQuery: FC<Props> = ({ ChildComponent, ErrorComponent }) => {
   const { data, error, loading } = useQuery<UserInfo>(GET_USER, {
     variables: {
       data: window.localStorage.getItem("userId"),
     },
   });
   if (loading) return <div>Загрузка...</div>;
-  if (error) return <div>Ошибка загрузки...</div>;
+  if (error) return <ErrorComponent />;
   return <ChildComponent getUser={data?.getUser as User} />;
 };
