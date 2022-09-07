381b3a425396182a6cc867462c8ed96ff0ce2eee
sazonov <fromlhz03@gmail.com>
2022-09-07 23:25:15 +0300

    rework login component

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,35 +1,52 @@
-import { ChangeEvent, FC, useState } from "react";
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { Register } from "src/graphql/dto/registerUser";
+import { Error } from "src/components/common/Error/Error";
 import { Container } from "../../components/Container/Container";
 import { Form } from "../../components/Form/Form";
-import { Input } from "../../components/Input/Input";
-import { LoginUser } from "../../graphql/dto/loginUser";
-import { getAuth } from "./getAuth";
+import { Input } from "../../components/common/Input/Input";
+import { useAuth } from "../../hooks/useAuth";
 
 export const Login: FC = () => {
-  const [loginFields, setLoginFields] = useState<LoginUser>({
-    email: "",
-    password: "",
+  const { getUser } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Register>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
   });
 
-  const { getUser } = getAuth({ email: loginFields.email.trim(), password: loginFields.password.trim() });
-
-  const changeLogin = (event: ChangeEvent<HTMLInputElement>) => {
-    const { target } = event;
-    const { value } = target;
-    setLoginFields((prevState) => ({ ...prevState, email: value }));
+  const onSubmit = (data: Register) => {
+    getUser({
+      variables: {
+        data: {
+          email: data.email,
+          password: data.password,
+        },
+      },
+    });
   };
-
-  const changePassword = (event: ChangeEvent<HTMLInputElement>) => {
-    const { target } = event;
-    const { value } = target;
-    setLoginFields((prevState) => ({ ...prevState, password: value }));
-  };
-
   return (
     <Container>
-      <Form btnText="Войти" onClick={getUser}>
-        <Input title="Логин" value={loginFields.email} onChange={changeLogin} type="text" />
-        <Input title="Пароль" value={loginFields.password} onChange={changePassword} type="password" />
+      <Form btnText="Войти" onClick={handleSubmit(onSubmit)}>
+        <Input
+          title="Электронный адрес"
+          label="email"
+          register={register}
+          type="text"
+          pattern={{
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "Неверный формат электронного адреса",
+          }}
+        />
+        {errors.email?.type === "required" && <Error message="Не указан электронный адрес!" />}
+        {errors.email?.message && <Error message={`${errors.email.message}!`} />}
+        <Input title="Пароль" label="password" register={register} type="password" autoComplete="true" />
       </Form>
     </Container>
   );
